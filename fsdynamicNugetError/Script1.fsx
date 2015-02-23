57bbef171bcf07dcc42d8ea1e1d4321781e21b8a
Aaron Brewbaker <brewbaker.aaron@gmail.com>
2015-02-22 23:36:24 -0500

    init

---
+++ fsdynamicNugetError/Script1.fsx
@@ -0,0 +1,8 @@
+﻿
+#r @"..\packages\FSharp.InteropServices.Dynamic.0.0.2-alpha\lib\net45\FSharp.InteropServices.Dynamic.dll"
+
+open FSharp.InteropServices.Dynamic
+
+let cudnn = Library(@"L:\cudnn\cudnn64_65.dll")
+
+printfn "%s" (cudnn?cudnnGetVersion())
